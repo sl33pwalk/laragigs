fdc8f64c52fb520714db17ea2ffefc7e2f16fc61
i1-Lobov44 <lobov0255@gmail.com>
2023-08-30 00:06:41 +0500

    first steps

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Models\Listing;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// All Listings
+Route::get('/', function () {
+    return view('listings', [
+        'heading' => 'Latest Listings',
+        'listings' => Listing::all()
+    ]);
+});
+
+
+// Single Listing
+Route::get('/listings/{listing}', function (Listing $listing) {
+    return view('listing', [
+        'listing' => $listing
+    ]);
+});
